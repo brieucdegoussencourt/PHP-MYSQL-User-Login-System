816cc87321f2292a8d4ef139ea56cbdbe56a3238
brieucdegoussencourt <brieuc.degoussencourt@gmail.com>
2024-07-29 15:34:43 +0200

    setting up login form

---
+++ READ.md
@@ -0,0 +1,3 @@
+LOGIN SIGNUP FORM TEMPLATE
+
+https://github.com/keerti1924/PHP-MYSQL-User-Login-System
--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+// Start the session to allow session variables to be used.
 session_start();
 ?>
 <!DOCTYPE html>
@@ -19,51 +20,55 @@ session_start();
       <?php
       include "connection.php";
 
+      // Check if the login form has been submitted.
       if (isset($_POST['login'])) {
 
+        // Retrieve email and password from POST request.
         $email = $_POST['email'];
         $pass = $_POST['password'];
 
+        // SQL query to fetch user data based on the provided email.
         $sql = "select * from users where email='$email'";
 
+        // Execute the SQL query.
         $res = mysqli_query($conn, $sql);
 
+        // Check if the query returned any results.
         if (mysqli_num_rows($res) > 0) {
 
+          // Fetch the associative array of the first result row.
           $row = mysqli_fetch_assoc($res);
 
+          // Get the password hash from the database.
           $password = $row['password'];
 
+          // Verify the provided password against the hashed password.
           $decrypt = password_verify($pass, $password);
 
-
+          // If the password is correct, set session variables and redirect to home.php.
           if ($decrypt) {
             $_SESSION['id'] = $row['id'];
             $_SESSION['username'] = $row['username'];
             header("location: home.php");
 
-
           } else {
+            // If the password is incorrect, display an error message.
             echo "<div class='message'>
                     <p>Wrong Password</p>
                     </div><br>";
-
             echo "<a href='login.php'><button class='btn'>Go Back</button></a>";
           }
 
         } else {
+          // If no user is found with the provided email, display an error message.
           echo "<div class='message'>
                     <p>Wrong Email or Password</p>
                     </div><br>";
-
           echo "<a href='login.php'><button class='btn'>Go Back</button></a>";
-
         }
 
-
       } else {
-
-
+        // If the form has not been submitted, display the login form.
         ?>
 
         <header>Login</header>
@@ -72,18 +77,20 @@ session_start();
 
           <div class="form-box">
 
-
+            <!-- Email input field with Font Awesome icon -->
             <div class="input-container">
               <i class="fa fa-envelope icon"></i>
               <input class="input-field" type="email" placeholder="Email Address" name="email">
             </div>
 
+            <!-- Password input field with Font Awesome icon and toggle for show/hide password -->
             <div class="input-container">
               <i class="fa fa-lock icon"></i>
               <input class="input-field password" type="password" placeholder="Password" name="password">
               <i class="fa fa-eye toggle icon"></i>
             </div>
 
+            <!-- Remember me checkbox and forgot password link -->
             <div class="remember">
               <input type="checkbox" class="check" name="remember_me">
               <label for="remember">Remember me</label>
@@ -92,10 +99,10 @@ session_start();
 
           </div>
 
-
-
+          <!-- Submit button for the form -->
           <input type="submit" name="login" id="submit" value="Login" class="button">
 
+          <!-- Link to signup page for users without an account -->
           <div class="links">
             Don't have an account? <a href="signup.php">Signup Now</a>
           </div>
@@ -107,6 +114,7 @@ session_start();
       ?>
   </div>
   <script>
+    // JavaScript to toggle the visibility of the password field.
     const toggle = document.querySelector(".toggle"),
       input = document.querySelector(".password");
     toggle.addEventListener("click", () => {
